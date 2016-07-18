f31978108609e3a518e43dd2f758599ebaff92cb
Bryce Carter <bryce.carte@gmail.com>
2016-07-18 16:03:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-"""
-grayscott.py
-~~~~~~~~~~~~
-
-Library for modeling a Gray-Scott reaction-diffusion
-system using numpy, scipy, and matplotlib. If problems
-arise from matplotlib's backend, either install QtPy, GTK,
-or try your luck with "template"...
-
-Usage only allows for variables in default_kwargs to be
-modified:
-
-    [n] is the size of the grid,
-    [f] is the feed rate of A into the system,
-    [k] is the kill rate of B from the system,
-    [dA/dB] are the rates of diffusion, typically with
-      ratio dA / dB = 2.
-
-The following are all valid uses of grayscott.py:
-    
-    $ python grayscott.py
-    $ python grayscott.py n=80
-    $ python grayscott.py f=0.031 k=0.059 n=80
-"""
-
 import sys
 import matplotlib
 
