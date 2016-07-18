8f1e46aa6238d1264e063a74d01ef7b20d3eeaf3
Bryce Carter <bryce.carte@gmail.com>
2016-07-18 16:01:18 -0400

    Reorganize module

--- grayscott.py
+++ grayscott.py
@@ -1,44 +1,8 @@
-"""
-grayscott.py
-~~~~~~~~~~~~
-
-Library for modeling a Gray-Scott reaction-diffusion
-system using numpy, scipy, and matplotlib. If problems
-arise from matplotlib's backend, either install QtPy, GTK,
-or try your luck with "template"...
-
-Usage only allows for variables in default_kwargs to be
-modified:
-
-    [n] is the size of the grid,
-    [f] is the feed rate of A into the system,
-    [k] is the kill rate of B from the system,
-    [dA/dB] are the rates of diffusion, typically with
-      ratio dA / dB = 2.
-
-The following are all valid uses of grayscott.py:
-    
-    $ python grayscott.py
-    $ python grayscott.py n=80
-    $ python grayscott.py f=0.031 k=0.059 n=80
-"""
-
-#### Libraries
-# Standard libraries
-import sys
-import math
 import random
-
-# Third-party libraries
 import numpy as np
-import matplotlib
-
-matplotlib.rcParams["backend"] = "Qt4Agg"
-
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from scipy import signal
-
 
 class GSSystem(object):
 
@@ -82,42 +46,3 @@ class GSSystem(object):
 
         anim = animation.FuncAnimation(fig, anim_func, interval=0)
         plt.show()
-
-
-def get_colormap():
-    if matplotlib.__version__ >= "1.5.1":
-        return plt.cm.magma
-    else:
-        return plt.cm.afmhot
-
-if __name__ == "__main__":
-
-    # Default values
-    default_kwargs = {
-        'n': 100,
-        'f': 0.016,
-        'k': 0.041,
-        'dA': 0.64,
-        'dB': 0.32
-    }
-
-    kwargs = default_kwargs.copy()
-
-    try:
-        f_args = map(lambda f_arg: f_arg.split("="), sys.argv[1:])
-
-        for key, val in f_args:
-            assert(key in default_kwargs.keys())
-            kwargs[key] = float(val)
-
-    except (TypeError, AssertionError, ValueError):
-        print "Bad usage, using default values."
-        kwargs = default_kwargs.copy()
-
-    cmap = get_colormap()
-    size = kwargs["n"]
-    seed_count = int(math.ceil(size * size / 900))
-
-    gs = GSSystem(**kwargs)
-    gs.seed(seed_count)
-    gs.animate(cmap)
